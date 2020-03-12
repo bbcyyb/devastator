ebac105d54919cdabd4da5d94f900dd339641fae
Kevin Yabing Yu <Kevin.Y.Yu@emc.com>
2020-03-12 23:17:14 +0800

    Add features module and add dashboard and daily-report sub module within features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }, {
+    path: 'dashboard',
+    loadChildren: () =>
+    import('./features/dashboard/dashboard.module').then(
+      m => m.DashboardModule
+    )
+  }, {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes, {
+      // TODO: need to understand what the parameters below are used for.
+      useHash: true,
+      scrollPositionRestoration: 'enabled',
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
