acaad33fea745a0615981bca31850a4479513b18
Kevin Yabing Yu <Kevin.Y.Yu@emc.com>
2020-03-16 22:40:57 +0800

    Add auth and content layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentLayoutComponent } from './content-layout/content-layout.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ContentLayoutComponent, AuthLayoutComponent, FooterComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
