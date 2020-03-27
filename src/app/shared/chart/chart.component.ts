39ef252e1e95cf4df3576853b741b4b8200525d5
Kevin Yabing Yu <Kevin.Y.Yu@emc.com>
2020-03-27 23:10:08 +0800

    Start coding follow the public repo on github

--- src/app/shared/chart/chart.component.ts
+++ src/app/shared/chart/chart.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  Input, 
+  OnChanges,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter,
+  NgZone} from '@angular/core';
+import {
+  ChartData,
+  ChartLegendOptions,
+  ChartOptions,
+  ChartType
+} from 'node_modules/chart.js';
+import { ChangeDetectionStrategy } from '@angular/compiler/src/core';
+
+declare var require: any;
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements AfterViewInit, OnChanges {
 
-  constructor() { }
+  chantInstance: any;
 
-  ngOnInit(): void {
-  }
+  @ViewChild('ref', {static: true}) ref: ElementRef<HTMLCanvasElement>;
 
+  @Output() chartClick = new EventEmitter<ChartClickEvent>();
+
+  @Input() type: ChartType | string = 'doughnut';
+  @Input() data: ChartData | any;
+  @Input() options: ChartOptions | any = {};
+
+  @Input() height = 150;
+  @Input() width = 300;
+  @Input() legend: ChartLegendOptions | any = {
+    display: true,
+    position: 'bottom'
+  };
+  @Input() plugins: any[];
+  @Input() redraw = false;
+  @Input() datasetKeyProvider: (x: any) => string = d => d.label;
+
+  constructor(private zone: NgZone) {}
+
+  ngAfterViewInit() {}
+
+  ngOnChanges(changes: any) {}
+}
+
+export interface ChartClickEvent {
+  elements: any[];
+  element: any;
+  dataset: any[];
+  $event: Event;
 }
